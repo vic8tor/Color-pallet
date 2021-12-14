9f4e593a4986c80914d9ac835513ee224ed132ae
vic8tor <timongrinni@gmail.com>
2021-12-14 11:35:28 +0300

    Initial Commit

---
+++ Sliders_RGB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sliders_RGB
+//
+//  Created by Victor on 14.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
